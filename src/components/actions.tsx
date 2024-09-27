f6891d45754d2e183315794f502a3547305c7d7a
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-09-27 15:37:33 -0300

    fix cache table

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -4,17 +4,14 @@ import { PatientColumns } from "./columns";
 export async function getPatients(): Promise<PatientColumns[]> {
   try {
     const res = await fetch("http://127.0.0.1:5000/patients", {
-    cache: 'no-store',
-  });
+      cache: "no-store",
+    });
 
     if (!res.ok) {
       throw new Error(`Error fetching patients: ${res.statusText}`);
     }
 
-    const data = await res.json();
-
-    const patients: PatientColumns[] = data.patients || [];
-
+    const patients: PatientColumns[] = await res.json();
     revalidatePath("/");
 
     return patients;
@@ -24,21 +21,21 @@ export async function getPatients(): Promise<PatientColumns[]> {
   }
 }
 
-
 export async function deletePatient(id: number) {
-const response = await fetch(`http://127.0.0.1:5000/patient/${id}`, {
-  method: "DELETE",
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
-  .then((res) => {
-    if (res.status == 200 || res.status == 204) {
-      alert("Predição Deletada!"); 
-      revalidatePath("/");
-    }
-    if (res.status == 500) {
-      alert("Erro ao deletar");
-    }
-  }).catch((e) => alert(`Erro ao deletar`));
+  const response = await fetch(`http://127.0.0.1:5000/patient/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.status == 200 || res.status == 204) {
+        alert("Predição Deletada!");
+        revalidatePath("/");
+      }
+      if (res.status == 500) {
+        alert("Erro ao deletar");
+      }
+    })
+    .catch((e) => alert(`Erro ao deletar`));
 }
